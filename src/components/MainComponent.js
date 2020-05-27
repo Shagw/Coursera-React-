403fce3495f7c4e45385d3e7c3e535a0acd70aa2
Shashank Agarwal <shashanka141@gmail.com>
2020-05-27 18:15:00 +0530

    React Router

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './MenuComponents';
 import DishDetail from './DishDetailComponent';
+import Home from './HomeComponent'
+import Header from './Header'
+import Footer from './Footer'
 import { DISHES } from '../shared/dishes';
+import {Switch, Route, Redirect} from 'react-router-dom';
 
 class Main extends Component {
 
@@ -10,31 +13,35 @@ class Main extends Component {
     super(props);
     this.state = {
         dishes: DISHES,
-        selectedDish: null
     };
   }
 
-  onDishSelect(dishId) {
-    this.setState({ selectedDish: dishId});
-  }
 
-   renderDish(dish){
-      if(dish!=null) return <DishDetail dish=
-      {this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
-      else return <div />
-      }
+  //  renderDish(dish){
+  //     if(dish!=null) return <DishDetail dish=
+  //     {this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
+  //     else return <div />
+  //     }
 
   render() {
     
+    const HomePage=()=>{
+      return(
+        <Home />
+      )
+    }
+    
     return (
       <div>
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-          </div>
-        </Navbar>
-        <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)} />
-        {this.renderDish(this.state.selectedDish)}
+        <Header />
+        <Switch>
+          <Route path="/home" component={HomePage} />
+          <Route exact path ="/menu" component={()=> <Menu dishes={this.state.dishes}/> }/>
+          <Redirect to="/home" />
+        </Switch>
+        {/* <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)} />
+        {this.renderDish(this.state.selectedDish)} */}
+        <Footer />
       </div>
     );
   }
